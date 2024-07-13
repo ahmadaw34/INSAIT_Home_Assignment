1ed851da70de7aa28ded19cee149e65df14fa63f
ahmadaw <ahmadaw@post.bgu.ac.il>
2024-07-13 11:00:15 +0300

    data base entity added

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Flask,request,redirect,jsonify
 import openai
+from flask_sqlalchemy import SQLAlchemy
+from QAEntity import QAEntity
 
 
 # openai.api_key='sk-proj-nrtKQE5d4qraTP3dWxY0T3BlbkFJWuzNFZamZupWyZ7WVJUt'
 app = Flask(__name__)
+db=SQLAlchemy()
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgre:ahmadaw@localhost/Question_Answer'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 @app.route('/ask', methods=['GET','POST'])
 def ask():
@@ -15,6 +21,10 @@ def ask():
             model='gpt-3.5-turbo',
             messages=[{"role": "assistant", "content": "capital of israel"}])
         answer = response.choices[0].message.content
+
+        q_a=QAEntity(question=question,answer=answer)
+        db.session.add(q_a)
+        db.session.commit()
 
         return jsonify({'question': question, 'answer': answer})
     
